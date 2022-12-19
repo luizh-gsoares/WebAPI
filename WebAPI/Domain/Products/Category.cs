4b2af8d38eaf96cbe856f6d1b95714c8d16e6460
luizh-gsoares <luizhenriquesoares18@hotmail.com>
2022-12-19 11:00:41 -0300

    added class

---
+++ WebAPI/Domain/Products/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Domain.Products;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set; }
+
+}
